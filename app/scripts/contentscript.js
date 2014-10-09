0c1c010d4a14145e89eb55b151298854e1d4d2f9
Johny Lam <johny.lam@lithium.com>
2014-10-09 13:36:04 -0700

    create fake submit button

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Content script');
+
+function getSubmitButton () {
+  return $('input[type="submit"][value="Post Your Comment"]');
+}
+
+function createFakeSubmitButton () {
+  var fakeSubmitButton, submitButton;
+  fakeSubmitButton = $('<input class="lia-button lia-button-primary lia-button-Submit-action" value="Post Your Comment" type="button">');
+  submitButton = getSubmitButton();
+
+  submitButton.hide();
+  submitButton.parent().append(fakeSubmitButton);
+
+  return fakeSubmitButton;
+}
+
+
+$(document).ready(function () {
+  createFakeSubmitButton();
+});
